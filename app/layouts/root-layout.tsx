384dfd25e2a81419cf5a6f93544990f7867e0a7c
avimonnuarnob <avimonnuarnob@gmail.com>
2025-05-26 17:31:30 +0600

    footer brand ambassadors and sponsor

--- app/layouts/root-layout.tsx
+++ app/layouts/root-layout.tsx
@@ -6,6 +6,111 @@ import Sidebar from "./Sidebar";
 import Topbar from "./Topbar";
 import type { RootLoaderData } from "@/root";
 
+import method1 from "@/assets/images/payment-method/pay16.png";
+import method2 from "@/assets/images/payment-method/pay17.png";
+import method3 from "@/assets/images/payment-method/pay22.png";
+import method4 from "@/assets/images/payment-method/pay33.png";
+import method5 from "@/assets/images/payment-method/pay34.png";
+import method6 from "@/assets/images/payment-method/pay45.png";
+import method7 from "@/assets/images/payment-method/pay46.png";
+import method8 from "@/assets/images/payment-method/pay59.png";
+import method9 from "@/assets/images/payment-method/pay60.png";
+import method0 from "@/assets/images/payment-method/pay61.png";
+
+import sponsor1 from "@/assets/images/sponsor/sponsor1.png";
+import sponsor2 from "@/assets/images/sponsor/sponsor2.png";
+import sponsor3 from "@/assets/images/sponsor/sponsor3.png";
+import sponsor4 from "@/assets/images/sponsor/sponsor4.png";
+import sponsor5 from "@/assets/images/sponsor/sponsor5.png";
+
+import ambassador1 from "@/assets/images/ambassador/ambassadors1.png";
+import ambassador2 from "@/assets/images/ambassador/ambassadors2.png";
+import ambassador3 from "@/assets/images/ambassador/ambassadors3.png";
+import ambassador4 from "@/assets/images/ambassador/ambassadors4.png";
+
+const PAYMENT_METHODS = [
+  method1,
+  method2,
+  method3,
+  method4,
+  method5,
+  method6,
+  method7,
+  method8,
+  method9,
+  method0,
+];
+
+const SPONSORS = [
+  {
+    id: 1,
+    name: "Chepauk Super Gillies",
+    title: "Principal Sponsor",
+    year: "2023",
+    icon: sponsor1,
+  },
+  {
+    id: 2,
+    name: "Saint Lucia Kings",
+    title: "Title Sponsor",
+    year: "2023 - 2024",
+    icon: sponsor2,
+  },
+  {
+    id: 3,
+    name: "Galle Titans",
+    title: "Main Sponsor",
+    year: "2023",
+    icon: sponsor3,
+  },
+  {
+    id: 4,
+    name: "Morrisville Samp Army",
+    title: "Title Sponsor",
+    year: "2023",
+    icon: sponsor4,
+  },
+  {
+    id: 5,
+    name: "Los Angeles Knight Riders",
+    title: "Principal Sponsor",
+    year: "2024",
+    icon: sponsor5,
+  },
+];
+
+const AMBASSADORS = [
+  {
+    id: 1,
+    name: "Robin Uthappa",
+    title: "Indian Cricket Legend",
+    year: "2023 - 2024",
+    icon: ambassador1,
+  },
+  {
+    id: 2,
+    name: "Srabanti Chatterjee",
+    title: "Heart of Bengali Cinema",
+    year: "2023 - Present",
+    icon: ambassador2,
+  },
+
+  {
+    id: 3,
+    name: "Pori Moni",
+    title: "Star of Dhallywood",
+    year: "2023 - 2025",
+    icon: ambassador3,
+  },
+
+  {
+    id: 4,
+    name: "Dinesh Karthik",
+    title: "The Indian Finisher",
+    year: "2024 - Present",
+    icon: ambassador4,
+  },
+];
 export default function RootLayout() {
   const [isFull, setIsFull] = useState(false);
 
@@ -42,16 +147,14 @@ const Footer = () => {
         <div className="mb-5.25">
           <h4 className="text-[13px] mb-3.5 font-bold">Payment Methods</h4>
           <div className="flex gap-2">
-            <img
-              src="https://img.c88rx.com/cx/h5/assets/images/footer/color-black/pay16.png?v=1744705193129&source=mcdsrc"
-              width={40}
-              height={15}
-            />
-            <img
-              src="https://img.c88rx.com/cx/h5/assets/images/footer/color-black/pay16.png?v=1744705193129&source=mcdsrc"
-              width={40}
-              height={15}
-            />
+            {PAYMENT_METHODS.map((method, idx) => (
+              <img
+                key={idx}
+                src={method}
+                loading="lazy"
+                className="w-10 h-3.75 object-contain"
+              />
+            ))}
           </div>
         </div>
 
@@ -80,111 +183,47 @@ const Footer = () => {
         <div className="mb-5">
           <h4 className="text-[13px] mb-4.5 font-bold">Sponsor</h4>
           <div className="flex gap-2">
-            <div className="flex gap-2 items-center">
-              <img
-                src="https://img.c88rx.com/cx/h5/assets/images/footer/sponsor1.png?v=1744705193129&source=mcdsrc"
-                width={35}
-                height={35}
-              />
-              <div className="text-black">
-                <p className="text-xs font-bold leading-3">
-                  Chepauk Super Gillies
-                </p>
-                <p className="text-xs font-light italic leading-3">
-                  Principal Sponsor
-                </p>
-                <p className="text-xs font-light italic leading-3">2023</p>
+            {SPONSORS.map((sponser) => (
+              <div className="flex gap-2 items-center" key={sponser.id}>
+                <img src={sponser.icon} width={35} height={35} />
+                <div className="text-black">
+                  <p className="text-xs font-bold leading-3">{sponser.name}</p>
+                  <p className="text-xs font-light italic leading-3">
+                    {sponser.title}
+                  </p>
+                  <p className="text-xs font-light italic leading-3">
+                    {sponser.year}
+                  </p>
+                </div>
               </div>
-            </div>
-            <div className="flex gap-2 items-center">
-              <img
-                src="https://img.c88rx.com/cx/h5/assets/images/footer/sponsor1.png?v=1744705193129&source=mcdsrc"
-                width={35}
-                height={35}
-              />
-              <div className="text-black">
-                <p className="text-xs font-bold leading-3">
-                  Chepauk Super Gillies
-                </p>
-                <p className="text-xs font-light italic leading-3">
-                  Principal Sponsor
-                </p>
-                <p className="text-xs font-light italic leading-3">2023</p>
-              </div>
-            </div>
-            <div className="flex gap-2 items-center">
-              <img
-                src="https://img.c88rx.com/cx/h5/assets/images/footer/sponsor1.png?v=1744705193129&source=mcdsrc"
-                width={35}
-                height={35}
-              />
-              <div className="text-black">
-                <p className="text-xs font-bold leading-3">
-                  Chepauk Super Gillies
-                </p>
-                <p className="text-xs font-light italic leading-3">
-                  Principal Sponsor
-                </p>
-                <p className="text-xs font-light italic leading-3">2023</p>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
 
         <div className="mb-5">
           <h4 className="text-[13px] mb-3.5 font-bold">Brand Ambassador</h4>
           <div className="flex gap-2">
-            <div className="">
-              <img
-                className="mb-1.5"
-                src="https://img.c88rx.com/cx/h5/assets/images/footer/ambassadors1.png?v=1744705193129&source=mcdsrc"
-                width={64}
-                height={32}
-              />
-              <div className="text-black ">
-                <p className="text-xs font-bold leading-3">Robin Uthappa</p>
-                <p className="text-[11px] font-light italic leading-2.75">
-                  Indian Cricket Legend
-                </p>
-                <p className="text-[11px] font-light italic leading-2.75">
-                  2023 - 2024
-                </p>
+            {AMBASSADORS.map((ambassador) => (
+              <div className="" key={ambassador.id}>
+                <img
+                  className="mb-1.5"
+                  src={ambassador.icon}
+                  width={64}
+                  height={32}
+                />
+                <div className="text-black ">
+                  <p className="text-xs font-bold leading-3">
+                    {ambassador.name}
+                  </p>
+                  <p className="text-[11px] font-light italic leading-2.75">
+                    {ambassador.title}
+                  </p>
+                  <p className="text-[11px] font-light italic leading-2.75">
+                    {ambassador.year}
+                  </p>
+                </div>
               </div>
-            </div>
-            <div className="">
-              <img
-                className="mb-1.5"
-                src="https://img.c88rx.com/cx/h5/assets/images/footer/ambassadors1.png?v=1744705193129&source=mcdsrc"
-                width={64}
-                height={32}
-              />
-              <div className="text-black ">
-                <p className="text-xs font-bold leading-3">Robin Uthappa</p>
-                <p className="text-[11px] font-light italic leading-2.75">
-                  Indian Cricket Legend
-                </p>
-                <p className="text-[11px] font-light italic leading-2.75">
-                  2023 - 2024
-                </p>
-              </div>
-            </div>
-            <div className="">
-              <img
-                className="mb-1.5"
-                src="https://img.c88rx.com/cx/h5/assets/images/footer/ambassadors1.png?v=1744705193129&source=mcdsrc"
-                width={64}
-                height={32}
-              />
-              <div className="text-black ">
-                <p className="text-xs font-bold leading-3">Robin Uthappa</p>
-                <p className="text-[11px] font-light italic leading-2.75">
-                  Indian Cricket Legend
-                </p>
-                <p className="text-[11px] font-light italic leading-2.75">
-                  2023 - 2024
-                </p>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
 
